aa31b19048bb781b41af46f39f2c8cf3cd1916ee
Steven Choi <steven@wicket.io>
2021-03-30 16:35:02 -0400

    snake follows closest food but does not recognize body

--- index.js
+++ index.js
@@ -45,44 +45,71 @@ function handleMove(request, response) {
   const boardLimit = gameData.board.height;
 
   let possibleMoves = ['up', 'right', 'down', 'left'];
+  let move;
 
   const closestFoods = findClosestFood(food, head);
   console.log('head:', head);
   console.log('my body:', gameData.you.body);
 
   for (let i = 0; i < closestFoods.length; i++) {
-    const moveToFood = moveTowardsClosestFood(closestFoods[i], head);
+    const moveToFood = moveTowardsClosestFood(closestFoods[i], head, snakeBody, boardLimit);
     const coord = moveAsCoord(moveToFood, head); 
     if (!offBoard(coord, boardLimit) && !coordEqual(coord, snakeBody)) {
-      console.log('MOVE:', moveToFood)
-      response.status(200).send({ move: moveToFood });
+      console.log('moveToFood:', moveToFood);
+      move = moveToFood;
+      console.log('MOVE:', move);
+      response.status(200).send({ move: move });
+      break;
+    } else {
+      // if the move is not valid:
+      // 1. delete original move
+      // 2. find the next best move
+      // 3. validate and pass move for returning
+      continue;
     }
   }
+  // response.status(200).send({ move: move });
 }
 
+// calculate distance from snake head
 function distanceFromHead(coord,head) {
-  const diffX = head.x - coord.x;
-  const diffY = head.y - coord.y;
-  return (diffX * diffX) + (diffY * diffY);
+  let diffX = head.x - coord.x;
+  let diffY = head.y - coord.y;
+
+  if (diffX < 0) {
+    diffX *= -1;
+  }
+
+  if (diffY < 0) {
+    diffY *= -1;
+  }
+
+  return (diffX + diffY);
 }
 
+// sort all foods in game by distance closest to snake head
 function findClosestFood(foodArray, head) {
   const foodByDistance = foodArray.sort(food => distanceFromHead(food, head));
   console.log('closest food:', foodByDistance[0]);
   return foodByDistance;
 }
 
-function moveTowardsClosestFood(food, head) {
-  let possibleMoves = ['up', 'right', 'down', 'left'];
-
+function moveTowardsClosestFood(food, head, snakeBody, boardLimit) {
   const distanceDiffX = food.x - head.x;
-  const distanceDiffY = head.y - food.y;
+  const distanceDiffY = food.y - head.y;
+  const position = { distanceDiffX, distanceDiffY }
+  console.log('move towards position:', position);
 
-  console.log('distanceDiffX:', distanceDiffX);
-  console.log('distanceDiffY:', distanceDiffY);
+  let possibleMoves = ['up', 'right', 'down', 'left']
+
+  // let possibleMoves = [moveAsCoord('up', head), moveAsCoord('right', head), moveAsCoord('down', head), moveAsCoord('left', head)];
+
+  let newPossibleMoves = possibleMoves.filter(move => !offBoard(moveAsCoord(move, head), boardLimit) && !coordEqual(moveAsCoord(move, head), snakeBody));
+
+  console.log('new possible moves:', newPossibleMoves);
 
   let diffX = food.x - head.x;
-  let diffY = head.y - food.y;
+  let diffY = food.y - head.y;
 
   // absolute difference in distance
   if (diffX < 0) {
@@ -95,17 +122,17 @@ function moveTowardsClosestFood(food, head) {
 
   if (diffX === 0) {
     if (distanceDiffY > 0) {
-      return 'down';
-    } else {
       return 'up';
+    } else {
+      return 'down';
     }
   } 
   
   if (diffY === 0) {
     if (distanceDiffX > 0) {
-      return 'left';
-    } else {
       return 'right';
+    } else {
+      return 'left';
     }
   }
 
@@ -121,8 +148,19 @@ function moveTowardsClosestFood(food, head) {
     } else {
       return 'down';
     }
-  } else {
-    return 'right';
+  } else if (diffX === diffY) {
+    // invalidate any moves that cannot be made
+    for (const move of newPossibleMoves) {
+      const coord = moveAsCoord(move, head);
+      if (offBoard(coord, boardLimit) || coordEqual(coord, snakeBody)) {
+        console.log(`${move} not valid! Moving on...`);
+        continue;
+      } else {
+      // if (distanceDiffY > 0 && (!offBoard(coord, boardLimit) && !coordEqual(coord, snakeBody))) {
+      // return move;
+      return move;
+      }
+    }
   }
 }
 
