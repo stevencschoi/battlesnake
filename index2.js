1dfd5b901d1edb4f399c701d05cf9649e1777f62
Steven Choi <steven@wicket.io>
2021-04-02 11:08:14 -0400

    added function to determine moves by most open space

--- index2.js
+++ index2.js
@@ -67,8 +67,10 @@ function handleMove(request, response) {
       headshot = getMoveTowardsTarget(possibleMoves, enemy.head, snakeBody);
       console.log('GOING FOR THE KILL:', headshot);
     }
-
   }
+
+  console.log('finding open space...');
+  findOpenSpace(possibleMoves, snakeBody, snakes, hazards, boardLimit);
 
   let bestMoveTowardsFood = getMoveTowardsTarget(possibleMoves, foodByDistance[0], snakeBody);
 
@@ -98,6 +100,104 @@ function handleMove(request, response) {
 }
 
 //! ********** decision making indicators **********
+//calculate open space
+function findOpenSpace(moves, snakeBody, snakes, hazards, boardLimit) {
+  // compare possible moves and determine which option will have the most space to move
+  // convert potential move to coordinate and sort them by available space (least hazards within 1 unit in surrounding area)
+  const head = snakeBody[0];
+  
+  let obstacles = snakes
+    .map(snake => snake.body)
+    .flat()
+    .concat(hazards);
+
+  let obstaclesSortedByDistance = sortTargetsByDistance(obstacles, head); 
+  obstaclesSortedByDistance.shift(); //! remove own head from array
+  console.log('obstacles sorted by distance:', obstaclesSortedByDistance);
+  
+  let movesByDistance = [];
+  for (let i = 0; i < moves.length; i++) {
+    // convert move to coordinate
+    // find all obstacles on the same axis (x,y)
+    // sort by the closest obstacle
+    // get diffX / diffY
+    // move towards the diff with higher absolute value
+    // if equal, move towards greater distance area
+    const theMoveAsCoord = moveAsCoord(moves[i], head);
+    console.log('the move:', theMoveAsCoord);
+    console.log('move:', moves[i]);
+
+    let obj = {
+      id: moves[i],
+      coord: theMoveAsCoord,
+    };
+
+    const obstaclesRelativeToMoveX = obstaclesSortedByDistance
+      .filter(obstacle => obstacle.x === moveAsCoord(moves[i], head).x)
+      .sort((a,b) => getDiffX(a, moveAsCoord(moves[i], head)) - getDiffX(b, moveAsCoord(moves[i], head)));
+
+    console.log('obstaclesRelativeToX:', obstaclesRelativeToMoveX);
+    const closestObstacleX = obstaclesRelativeToMoveX[0];
+    console.log('closest obstacle on x axis:', closestObstacleX);
+    
+    if (closestObstacleX !== undefined) {
+      let lowestDiffY = getDiffY(closestObstacleX, moveAsCoord(moves[i], head));
+      let absoluteDiffY;
+
+      if (lowestDiffY < 0) {
+        absoluteDiffY = lowestDiffY * -1;
+      } else {
+        absoluteDiffY = lowestDiffY;
+      }
+
+      obj.lowestDiffY = lowestDiffY;
+      obj.absoluteDiffY = absoluteDiffY;
+
+    } else {
+      obj.lowestDiffY = theMoveAsCoord.y - boardLimit - 1;
+      if (obj.lowestDiffY < 0) {
+        obj.absoluteDiffY = obj.lowestDiffY * -1;
+      } else {
+        obj.absoluteDiffY = obj.lowestDiffY;
+      }
+    }
+
+    const obstaclesRelativeToMoveY = obstaclesSortedByDistance
+      .filter(obstacle => obstacle.y === moveAsCoord(moves[i], head).y)
+      .sort((a,b) => getDiffY(a, moveAsCoord(moves[i], head)) - getDiffY(b, moveAsCoord(moves[i], head)));
+
+    console.log('obstaclesRelativeToY:', obstaclesRelativeToMoveY);
+    const closestObstacleY = obstaclesRelativeToMoveY[0];
+    console.log('closest obstacle on y axis:', closestObstacleY);
+    
+    if (closestObstacleY !== undefined) {
+      let lowestDiffX = getDiffX(closestObstacleY, moveAsCoord(moves[i], head));
+      let absoluteDiffX;
+      if (lowestDiffX < 0) {
+        absoluteDiffX = lowestDiffX * -1;
+      } else {
+        absoluteDiffX = lowestDiffX;
+      }
+
+      obj.lowestDiffX = lowestDiffX;
+      obj.absoluteDiffX = absoluteDiffX;
+    } else {
+      obj.lowestDiffX = theMoveAsCoord.x - boardLimit - 1;
+      if (obj.lowestDiffX < 0) {
+        obj.absoluteDiffX = obj.lowestDiffX * -1;
+      } else {
+        obj.absoluteDiffX = obj.lowestDiffX;
+      }
+    }
+    obj.mostSpace = obj.absoluteDiffY * obj.absoluteDiffX;
+    movesByDistance.push(obj);
+  }
+  movesByDistance.sort((a,b) => b.mostSpace - a.mostSpace);
+  console.log('moves by distance:', movesByDistance);
+  console.log('move towards most space:', movesByDistance[0].id, movesByDistance[0].mostSpace);
+  return movesByDistance;
+}
+
 // calculate distance from snake head
 function getDiffX(coord, head) {
   return head.x - coord.x;
