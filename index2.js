dd2ca3b78eeb64dd6db64210e4d11145eb9b25af
Steven Choi <steven@wicket.io>
2021-04-02 00:49:28 -0400

    snake only dies when trapped out of moves

--- index2.js
+++ index2.js
@@ -38,34 +38,146 @@ function handleMove(request, response) {
   const head = gameData.you.head;
   const neck = gameData.you.body[1];
   const snakeBody = gameData.you.body;
-  const tail = gameData.you.body[gameData.you.body.length-1];
-  // let snakes = gameData.board.snakes.map(snake => snake.body).flat();
   const snakes = gameData.board.snakes;
-  console.log('snakes:', snakes);
   const food = gameData.board.food;
   const hazards = gameData.board.hazards;
   const boardLimit = gameData.board.height;
-  console.log('snakes at turn start:', snakes);
+  // console.log('snakes at turn start:', snakes);
 
   let moves = ['up', 'right', 'down', 'left'];
   // eliminate possible moves that are out of bounds or part of a snake
   let possibleMoves = filterPossibleMoves(moves, boardLimit, snakeBody, snakes);
 
   console.log('possibleMoves:', possibleMoves);
+
+  const foodByDistance = sortTargetsByDistance(food, head);
+
+  console.log('food by distance:', foodByDistance);
   
-  let move = possibleMoves[0];
+  let move;
 
   if (possibleMoves.length === 1) {
     move = possibleMoves[0];
   }
 
-  console.log('current direction:', getCurrentDirection(head, neck));
-  console.log('moving to:', move);
+  let bestMoveTowardsFood = getMoveTowardsTarget(possibleMoves, foodByDistance[0], snakeBody);
+
+  console.log('best move towards food:', bestMoveTowardsFood);
+
+  move = bestMoveTowardsFood;
+
+  // console.log('current direction:', getCurrentDirection(head, neck));
+  console.log('confirming move:', move);
 
   response.status(200).send({
     move: move,
     shout: 'serenity now!'
   });
+}
+
+//! ********** decision making indicators **********
+// calculate distance from snake head
+function getDiffX(coord, head) {
+  return head.x - coord.x;
+}
+
+function getDiffY(coord, head) {
+  return head.y - coord.y;
+}
+
+function getDistanceFromHead(coord,head) {
+  let diffX = getDiffX(coord, head);
+  let diffY = getDiffY(coord, head);
+
+  if (diffX < 0) {
+    diffX *= -1;
+  }
+
+  if (diffY < 0) {
+    diffY *= -1;
+  }
+
+  // console.log('diffX:', diffX);
+  // console.log('diffY:', diffY);
+  // console.log('distance from head:', diffX + diffY);
+  return diffX + diffY;
+}
+
+function sortTargetsByDistance(array, head) {
+  const arrSortedByDistance = array.sort((a,b) => getDistanceFromHead(a, head) - getDistanceFromHead(b, head));
+  console.log('closest target:', arrSortedByDistance[0]);
+  return arrSortedByDistance;
+}
+
+function getMoveTowardsTarget(moves, target, snakeBody) {
+  const directionMatrix = {
+    left: 'right',
+    up: 'down',
+    right: 'left',
+    down: 'up'
+  };
+
+  const head = snakeBody[0];
+  const neck = snakeBody[1];
+  // of possible moves, determine which move will get to food in minimum steps
+  const currentDirection = getCurrentDirection(head, neck);
+  const oppositeDirection = directionMatrix[currentDirection];
+
+  const newMoves = moves.filter(move => move !== oppositeDirection);
+
+  //? if positive diffX, coord is to the LEFT of head
+  //? if negative diffX, coord is to the RIGHT of head
+  //? if positive diffY, coord is to the DOWN of head
+  //? if negative diffY, coord is to the UP of head
+
+  console.log('possible moves in getMoveFn:', newMoves);
+
+  let sortedMovesByDistanceFromHead = newMoves.sort((a,b) => getDistanceFromHead(moveAsCoord(a, head), target) - getDistanceFromHead(moveAsCoord(b, head), target));
+
+  console.log('sortedMoves:', sortedMovesByDistanceFromHead);
+
+  return sortedMovesByDistanceFromHead[0];
+
+};
+
+//! ********** identity functions **********
+function moveAsCoord(move, head) {
+  switch(move) {
+    case 'up':
+      return {x: head.x, y: head.y+1};
+    case 'down':
+      return {x: head.x, y:head.y-1};
+    case 'left':
+      return {x: head.x-1, y: head.y};
+    case 'right':
+      return {x: head.x+1, y: head.y};
+  }
+}
+
+function getCurrentDirection(head, neck) {
+  if (head.x - neck.x === 0 && head.y - neck.y === -1) {
+    return 'down';
+  } else if (head.x - neck.x === 0 && head.y - neck.y === 1) {
+    return 'up';
+  } else if (head.x - neck.x === -1 && head.y - neck.y === 0) {
+    return 'left';
+  } else return 'right';
+};
+
+//! ********** validation tests **********
+function offBoard(position, boardLimit) {
+  if (position.x > boardLimit - 1 || position.x < 0) {
+    return true;
+  }
+  else if (position.y > boardLimit - 1 || position.y < 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function areCoordsEqual(coord, compareCoord) {
+  return coord.x === compareCoord.x && coord.y === compareCoord.y;
 }
 
 function filterPossibleMoves(array, boardLimit, snakeBody, snakes) {
@@ -81,67 +193,6 @@ function filterPossibleMoves(array, boardLimit, snakeBody, snakes) {
   });
 
   return possibleMoves;
-}
-
-function getCurrentDirection(head, neck) {
-  if (head.x - neck.x === 0 && head.y - neck.y === -1) {
-    return 'down';
-  } else if (head.x - neck.x === 0 && head.y - neck.y === 1) {
-    return 'up';
-  } else if (head.x - neck.x === -1 && head.y - neck.y === 0) {
-    return 'left';
-  } else return 'right';
-};
-
-// calculate distance from snake head
-function getDistanceFromHead(coord,head) {
-  // if positive diffX, coord is to the LEFT of head
-  // if negative diffX, coord is to the RIGHT of head
-  // if positive diffY, coord is to the DOWN of head
-  // if negative diffY, coord is to the UP of head
-  let diffX = head.x - coord.x;
-  let diffY = head.y - coord.y;
-
-  if (diffX < 0) {
-    diffX *= -1;
-  }
-
-  if (diffY < 0) {
-    diffY *= -1;
-  }
-
-  return (diffX + diffY);
-}
-
-function moveAsCoord(move, head) {
-  switch(move) {
-    case 'up':
-      return {x: head.x, y: head.y+1};
-    case 'down':
-      return {x: head.x, y:head.y-1};
-    case 'left':
-      return {x: head.x-1, y: head.y};
-    case 'right':
-      return {x: head.x+1, y: head.y};
-  }
-}
-
-function offBoard(position, boardLimit) {
-  if (position.x > boardLimit - 1 || position.x < 0) {
-    console.log(`{ ${position.x}, ${position.y} } is off board!`);
-    return true;
-  }
-  else if (position.y > boardLimit - 1 || position.y < 0) {
-    console.log(`{ ${position.x}, ${position.y} } is off board!`);
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function areCoordsEqual(coord, compareCoord) {
-  console.log('are coords equal?:', coord.x === compareCoord.x && coord.y === compareCoord.y);
-  return coord.x === compareCoord.x && coord.y === compareCoord.y;
 }
 
 function handleEnd(request, response) {
